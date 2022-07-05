6d88966c1f2c3f93e07d13d84b7ef424890bcfe6
bashang <bassam.abd.basir@gmail.com>
2022-07-05 12:05:28 +0800

    Add BaseTest class

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,18 @@
+package base;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setup(){
+
+    }
+
+    @AfterSuite
+    public void cleanup(){
+
+    }
+}
