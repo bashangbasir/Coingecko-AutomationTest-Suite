52ddf97a58221ac45d0bbd9d422b3e2b81258c22
bashang <bassam.abd.basir@gmail.com>
2022-07-05 12:05:13 +0800

    class for URI constant used

---
+++ src/main/java/enums/URIConstant.java
@@ -0,0 +1,10 @@
+package enums;
+
+public class URIConstant {
+
+    public static final String BASE_URI = "https://api.coingecko.com/api/v3";
+
+    public static final String PING_URI = BASE_URI + "/ping";
+
+
+}
