2a89ef47b45e6b14582bcc6d05328857a1ef1b0e
bashang <bassam.abd.basir@gmail.com>
2022-07-05 21:34:34 +0800

    Add test cases for ping endpoint.

--- src/test/java/tests/PingTest.java
+++ src/test/java/tests/PingTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static enums.URIConstant.PING_URI;
@@ -11,7 +12,7 @@ import static org.hamcrest.Matchers.*;
 public class PingTest extends BaseTest {
 
     @Test
-    public void PING_expect_200_Status(){
+    public void PING_checkStatusCode_expectHttp200(){
         given()
                 .when()
                     .get(PING_URI)
@@ -26,5 +27,14 @@ public class PingTest extends BaseTest {
                     .get(PING_URI)
                 .then()
                     .assertThat().body("'gecko_says'",equalTo("(V3) To the Moon!"));
+    }
+
+    @Test
+    public void PING_checkContentType_expectApplicationJson(){
+        given()
+                .when()
+                    .get(PING_URI)
+                .then()
+                    .assertThat().contentType(ContentType.JSON);
     }
 }
