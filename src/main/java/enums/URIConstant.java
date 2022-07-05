f3b7d7a33119f792fd7dae4f15742869f1325796
bashang <bassam.abd.basir@gmail.com>
2022-07-05 21:34:51 +0800

    Add TRENDING endpoint URI

--- src/main/java/enums/URIConstant.java
+++ src/main/java/enums/URIConstant.java
@@ -5,6 +5,6 @@ public class URIConstant {
     public static final String BASE_URI = "https://api.coingecko.com/api/v3";
 
     public static final String PING_URI = BASE_URI + "/ping";
-
+    public static final String TRENDING_URI = BASE_URI + "/search/trending";
 
 }
