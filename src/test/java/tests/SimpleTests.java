5e14ce1450607e794004bef5767729570057b581
bashang <bassam.abd.basir@gmail.com>
2022-07-20 23:45:52 +0800

    Add new test cases

--- src/test/java/tests/SimpleTests.java
+++ src/test/java/tests/SimpleTests.java
@@ -2,8 +2,11 @@ package tests;
 
 import base.BaseTest;
 import base.Data;
+import utils.CommonUtils;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static constants.URIConstant.SIMPLE_URI;
 import static io.restassured.RestAssured.given;
@@ -11,9 +14,8 @@ import static org.hamcrest.Matchers.hasKey;
 
 public class SimpleTests extends BaseTest {
 
-
     @Test(dataProvider = "dataSingleCoinVsSingleCurrency", dataProviderClass = Data.class)
-    public void SIMPLE_getSimplePrice_requiredQueryParamOnly(String coinId, String currency){
+    public void SIMPLE_getSimplePrice_singleCoinVsSingleCurrency_requiredQueryParamOnly(String coinId, String currency){
             given()
                     //Required query param
                     .queryParam("ids",coinId).queryParam("vs_currencies",currency)
@@ -29,7 +31,7 @@ public class SimpleTests extends BaseTest {
     }
 
     @Test(dataProvider = "dataSingleCoinVsSingleCurrency", dataProviderClass = Data.class)
-    public void SIMPLE_getSimplePrice_additionalQueryParam(String coinId, String currency){
+    public void SIMPLE_getSimplePrice_singleCoinVsSingleCurrency_additionalQueryParam(String coinId, String currency){
         given()
                 //Required query param
                 .queryParam("ids",coinId).queryParam("vs_currencies",currency)
@@ -52,4 +54,50 @@ public class SimpleTests extends BaseTest {
                 .assertThat().body(coinId,hasKey(currency+"_24h_change"))
                 .assertThat().body(coinId,hasKey("last_updated_at"));
     }
+
+    @Test(dataProvider = "dataSingleCoinVsMultipleCurrencies", dataProviderClass = Data.class)
+    public void SIMPLE_getSimplePrice_singleCoinVsMultipleCurrencies_requiredQueryParamOnly(String coinId, List currencies){
+
+        String vsCurrencies = CommonUtils.parseListToStringCommaSeparated(currencies);
+
+        given()
+                //Required query param
+                .queryParam("ids",coinId).queryParam("vs_currencies",vsCurrencies)
+                .when()
+                .get(SIMPLE_URI)
+                .then()
+                .assertThat().statusCode(200)
+                .assertThat().contentType(ContentType.JSON)
+                //Verify body contains ids field
+                .assertThat().body("",hasKey(coinId))
+                //Verify each coin contains all currency field
+                .assertThat().body(coinId,hasKey((String) currencies.get(0)))
+                .assertThat().body(coinId,hasKey((String) currencies.get(1)))
+                .assertThat().body(coinId,hasKey((String) currencies.get(2)));
+    }
+
+    @Test(dataProvider = "dataMultipleCoinVsSingleCurrencies", dataProviderClass = Data.class)
+    public void SIMPLE_getSimplePrice_multipleCoinsVsSingleCurrencies_requiredQueryParamOnly(List coins, String currency){
+
+        String coinIds = CommonUtils.parseListToStringCommaSeparated(coins);
+
+        given()
+                //Required query param
+                .queryParam("ids",coinIds)
+                .queryParam("vs_currencies",currency)
+                .when()
+                .get(SIMPLE_URI)
+                .then()
+                .assertThat().statusCode(200)
+                .assertThat().contentType(ContentType.JSON)
+                //Verify body contains ids field
+                .assertThat().body("",hasKey(coins.get(0)))
+                .assertThat().body("",hasKey(coins.get(1)))
+                .assertThat().body("",hasKey(coins.get(2)))
+                //Verify each coin contains all currency field
+                .assertThat().body((String)coins.get(0),hasKey(currency))
+                .assertThat().body((String)coins.get(1),hasKey(currency))
+                .assertThat().body((String)coins.get(2),hasKey(currency));
+    }
+
 }
