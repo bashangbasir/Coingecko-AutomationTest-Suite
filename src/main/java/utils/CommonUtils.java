22876932c475f429270c4af366434b04cdf27bd1
bashang <bassam.abd.basir@gmail.com>
2022-07-20 23:45:28 +0800

    Add CommonUtils class for utilities method use in the test cases

---
+++ src/main/java/utils/CommonUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.List;
+
+public class CommonUtils {
+
+    public static String parseListToStringCommaSeparated(List list){
+        return String.join(",",list);
+    }
+
+
+}
