b22005cbb8dabc75e89339f4da9b5fe4eca92ddc
bashang <bassam.abd.basir@gmail.com>
2022-07-30 01:17:17 +0800

    Refactor all the structure. Add module for web and mobile

---
+++ coingecko-web/src/test/java/base/BaseTest.java
@@ -0,0 +1,17 @@
+package base;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setup(){
+
+    }
+
+    @AfterSuite
+    public void cleanup(){
+
+    }
+}
