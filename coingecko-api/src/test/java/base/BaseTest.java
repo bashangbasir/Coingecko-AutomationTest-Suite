20e3c836b7026c48aa2c2cd8eef5d0aa32db002c
bashang <bassam.abd.basir@gmail.com>
2022-08-05 23:16:39 +0800

    Refarctor BaseTest for api. add response spec.

--- coingecko-api/src/test/java/base/BaseTest.java
+++ coingecko-api/src/test/java/base/BaseTest.java
@@ -1,14 +1,28 @@
 package base;
 
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
+    public static ResponseSpecification statusCode200responseSpec;
+    public static ResponseSpecification statusCode400responseSpec;
+    @BeforeClass
+    public static void createStatusCode200ResponseSpecs(){
+        statusCode200responseSpec = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                build();
+    }
 
     @BeforeClass
-    public void setup(){
-
+    public static void createStatusCode400ResponseSpecs(){
+        statusCode400responseSpec = new ResponseSpecBuilder().
+                expectStatusCode(400).
+                expectContentType(ContentType.JSON).
+                build();
     }
 
     @AfterSuite
