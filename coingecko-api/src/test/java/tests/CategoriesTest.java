41846157cc62c7ea0c065abc27d778eec6b6968e
bashang <bassam.abd.basir@gmail.com>
2022-08-05 23:19:21 +0800

    Add new test and refactor existing code in the class

--- coingecko-api/src/test/java/tests/CategoriesTest.java
+++ coingecko-api/src/test/java/tests/CategoriesTest.java
@@ -1,10 +1,11 @@
 package tests;
 
 import base.BaseTest;
-import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static constants.URIConstant.CATEGORIES_URI;
+import static constants.URIConstant.*;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -12,16 +13,31 @@ import static org.hamcrest.Matchers.*;
 public class CategoriesTest extends BaseTest {
 
     @Test
-    public void CATEGORIES_getListOfCategories(){
+    public void CATEGORIES_getListAllCategories(){
         given()
                 .when()
-                .get(CATEGORIES_URI)
+                .get(LIST_CATEGORIES_URI)
                 .then()
-                .assertThat().statusCode(200)
-                .assertThat().contentType(ContentType.JSON)
+                .spec(statusCode200responseSpec)
                 //Verify body has name and catogery_id field.
                 .assertThat().body("[0]",hasKey("name"))
                 .assertThat().body("[0]",hasKey("category_id"));
+    }
+
+    @Test
+    public void CATEGORIES_getListAllCategoriesWithMarketDatawithDefaultOrder(){
+        Response response = given()
+                .when()
+                .get(CATEGORIES_URI)
+                .then()
+                .spec(statusCode200responseSpec)
+                .extract().response();
+
+        //market_cap_desc by default , so verify the market is in descending order
+        float firstCategoryMarketCap = response.jsonPath().get("[0].market_cap");
+        float secondCategoryMarketCap = response.jsonPath().get("[1].market_cap");
+        float thirdCategoryMarketCap = response.jsonPath().get("[2].market_cap");
+        Assert.assertTrue((firstCategoryMarketCap > secondCategoryMarketCap) && (secondCategoryMarketCap >  thirdCategoryMarketCap));
     }
 
 
