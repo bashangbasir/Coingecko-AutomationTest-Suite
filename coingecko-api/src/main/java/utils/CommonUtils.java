3e64965bc65372cf6fac5b3651c7d5eb87e4c648
bashang <bassam.abd.basir@gmail.com>
2022-08-07 23:07:47 +0800

    Add getRandomCharacters methods .

--- coingecko-api/src/main/java/utils/CommonUtils.java
+++ coingecko-api/src/main/java/utils/CommonUtils.java
@@ -1,11 +1,32 @@
 package utils;
 
 import java.util.List;
+import java.util.Random;
 
 public class CommonUtils {
 
-    public static String parseListToStringCommaSeparated(List list){
-        return String.join(",",list);
+    public static String parseListToStringCommaSeparated(List list) {
+        return String.join(",", list);
+    }
+
+    public static String getRandomCharacters(int length) {
+        StringBuilder rndString = new StringBuilder();
+
+        while (rndString.length() < length) {
+            int index = (new Random()).nextInt("ABCDEFGHIJKLMNOPQRSTUVWXYZ".length());
+            rndString.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(index));
+        }
+        return rndString.toString().toString();
+    }
+
+    public static String getRandomCharacters() {
+        StringBuilder rndString = new StringBuilder();
+
+        while (rndString.length() < 5) {
+            int index = (new Random()).nextInt("ABCDEFGHIJKLMNOPQRSTUVWXYZ".length());
+            rndString.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(index));
+        }
+        return rndString.toString().toString();
     }
 
 
