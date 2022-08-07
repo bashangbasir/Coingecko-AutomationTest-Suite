71afc50f3c70e189d275d1a4f08837b1c2173221
bashang <bassam.abd.basir@gmail.com>
2022-08-07 23:26:10 +0800

    fix spaces in URIConstants

--- coingecko-api/src/main/java/constants/URIConstant.java
+++ coingecko-api/src/main/java/constants/URIConstant.java
@@ -6,7 +6,7 @@ public class URIConstant {
     public static final String PING_URI = BASE_URI + "/ping";
     public static final String TRENDING_URI = BASE_URI + "/search/trending";
     public static final String LIST_CATEGORIES_URI = BASE_URI + "/coins/categories/list";
-    public static final String CATEGORIES_URI = BASE_URI +"/coins/categories";
+    public static final String CATEGORIES_URI = BASE_URI + "/coins/categories";
     public static final String SIMPLE_URI = BASE_URI + "/simple/price";
 
 
