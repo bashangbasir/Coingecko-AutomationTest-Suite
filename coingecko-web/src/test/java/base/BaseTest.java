5cb6e91dc2b5ed72cf54b901a467e7813d5b4561
bashang <bassam.abd.basir@gmail.com>
2022-08-16 23:41:03 +0800

    Add BaseTest.java for web automation

--- coingecko-web/src/test/java/base/BaseTest.java
+++ coingecko-web/src/test/java/base/BaseTest.java
@@ -1,17 +1,35 @@
 package base;
 
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+import pages.HomePage;
 
 public class BaseTest {
 
-    @BeforeClass
-    public void setup(){
+    private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeMethod
+    @Parameters({"URL", "browser"})
+    public void setUp(String URL, String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+
+        homePage = new HomePage(driver);
+
+        driver.manage().window().maximize();
+        driver.get(URL);
     }
 
-    @AfterSuite
-    public void cleanup(){
-
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
