a3eb0be17463e3ca112f74a29a21f04c8dc863da
bashang <bassam.abd.basir@gmail.com>
2022-08-17 08:54:16 +0800

    - add clearCookiesModal method - add web element for CookiesModal and CookiesModalOkayBtn

--- coingecko-web/src/main/java/pages/HomePage.java
+++ coingecko-web/src/main/java/pages/HomePage.java
@@ -1,9 +1,9 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 import static constants.StringConstant.HomePageTitle;
@@ -11,20 +11,35 @@ import static constants.StringConstant.HomePageTitle;
 public class HomePage {
 
     private WebDriver driver;
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    //WEBELEMENT IN HOMEPAGE SCREEN
 
     @FindBy(xpath = "")
     private WebElement coinGeckoLogo;
 
     @FindBy(xpath = "")
     private WebElement candyNotificationIcon;
+    @FindBy(id = "cookie-notice")
+    private WebElement cookieModal ;
+    @FindBy(xpath = "//*[@id='cookie-notice']//button[@data-action='click->cookie-note#accept']")
+    private WebElement cookieModalOkayBtn ;
 
-    public HomePage(WebDriver driver){
-        this.driver = driver;
-    }
+    // ACTIONS IN HOMEPAGE
 
     public HomePage verifyHomePageTittle(){
        Assert.assertEquals(driver.getTitle() , HomePageTitle);
        return this;
+    }
+
+    public HomePage clearCookiesModal(){
+        if(cookieModal.isDisplayed()){
+            cookieModalOkayBtn.click();
+        }
+        return this;
     }
 
 
--- coingecko-web/src/test/java/tests/LoadPageTest.java
+++ coingecko-web/src/test/java/tests/LoadPageTest.java
@@ -1,18 +1,15 @@
 package tests;
 
 import base.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.HomePage;
-
-import static constants.StringConstant.HomePageTitle;
 
 public class LoadPageTest extends BaseTest {
 
     @Test
-    public void loadCoingeckoHompage(){
+    public void loadCoinGeckoHomepage(){
 
         homePage.
+                clearCookiesModal().
                 verifyHomePageTittle();
 
     }
