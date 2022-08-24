b754c22bf064f24e65d3cad04f4867afdc381bab
bashang <bassam.abd.basir@gmail.com>
2022-08-24 09:45:20 +0800

    Remove unused import

--- coingecko-web/src/test/java/base/BaseTest.java
+++ coingecko-web/src/test/java/base/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import pages.HomePage;
 
