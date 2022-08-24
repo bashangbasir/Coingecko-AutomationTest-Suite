3d733545ea86db9215fd3bfbcba87526a7d663a9
bashang <bassam.abd.basir@gmail.com>
2022-08-24 09:44:00 +0800

    Add sendGet methods and sendPost under the BaseTest class.

--- coingecko-api/src/test/java/base/BaseTest.java
+++ coingecko-api/src/test/java/base/BaseTest.java
@@ -2,9 +2,14 @@ package base;
 
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class BaseTest {
     public static ResponseSpecification statusCode200responseSpec;
@@ -29,5 +34,28 @@ public class BaseTest {
     @AfterSuite
     public void cleanup() {
 
+    }
+
+    public Response sendGet(String url) {
+        return given()
+                .when()
+                .get(url)
+                .then()
+                .spec(statusCode200responseSpec)
+                .extract()
+                .response();
+    }
+
+    public Response sendGet(String url, Map<String, Object> queryParams) {
+        return given()
+                .params(queryParams)
+                .when()
+                .get(url).then()
+                .spec(statusCode200responseSpec)
+                .extract().response();
+    }
+
+    public Response sendPost(String url, Object body) {
+        return given().when().post(url, body);
     }
 }
