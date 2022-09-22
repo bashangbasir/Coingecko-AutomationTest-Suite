0512939d7a9f77013fc27db06926a81ad3f6ffb0
bashang <bassam.abd.basir@gmail.com>
2022-09-22 23:24:19 +0800

    Add constants text for validation

--- coingecko-web/src/main/java/constants/StringConstant.java
+++ coingecko-web/src/main/java/constants/StringConstant.java
@@ -6,5 +6,8 @@ public class StringConstant {
     public static final String HOMEPAGE_TABTITTLE_TEXT = "Cryptocurrency Prices, Charts, and Crypto Market Cap | CoinGecko";
     public static final String SIGNED_IN_SUCCESS_TEXT = "Signed in successfully.";
     public static final String SIGNED_OUT_SUCCESS_TEXT = "Signed out successfully.";
+    public static final String SIGN_IN_TABTITTLE_TEXT = "Sign in to CoinGecko Dashboard | CoinGecko";
+    public static final String SIGN_IN_INVALID_EMAIL_PASSWORD_TEXT = "Invalid Email or password.";
+    public static final String SIGN_IN_UNCONFIRM_EMAIL_TEXT = "You have to confirm your email address before continuing.";
 
 }
