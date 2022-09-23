43325ab5a500bdfa7e91308793f667488fcdcc1b
bashang <bassam.abd.basir@gmail.com>
2022-09-24 00:53:30 +0800

    Add prefix to the test case

--- coingecko-web/src/test/java/tests/LoadPageTest.java
+++ coingecko-web/src/test/java/tests/LoadPageTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class LoadPageTest extends BaseTest {
 
     @Test
-    public void loadCoinGeckoHomepage() {
+    public void LOAD_loadCoinGeckoHomepage() {
 
         homePage.
                 clearCookiesModal().
