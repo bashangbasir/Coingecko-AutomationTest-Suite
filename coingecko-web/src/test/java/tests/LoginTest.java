1b531ad262339624e7e0ddb035504ebece3b349b
bashang <bassam.abd.basir@gmail.com>
2023-07-09 22:11:48 +0800

    Update test cases with user and dataProvider

--- coingecko-web/src/test/java/tests/LoginTest.java
+++ coingecko-web/src/test/java/tests/LoginTest.java
@@ -1,36 +1,36 @@
 package tests;
 
 import base.BaseTest;
+import constants.Data;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-    @Test
-    public void LOGIN_userLoginWithValidDetails() {
+    @Test(dataProvider = "validUser", dataProviderClass = Data.class)
+    public void LOGIN_userLoginWithValidDetails(String email, String password) {
 
         homePage.clearCookiesModal()
                 .verifyHomePageTittle()
-                .userLoginWithValidData("confirmemailtester@gmail.com","TestPassword@123")
+                .userLoginWithValidData(email, password)
                 .verifySignedInMessageSuccess()
                 .userSignOut()
                 .verifySignedOutMessageSuccess();
     }
 
-    @Test
-    public void LOGIN_userLoginWithInvalidDetails(){
+    @Test(dataProvider = "invalidUser",  dataProviderClass = Data.class)
+    public void LOGIN_userLoginWithInvalidDetails(String email, String password) {
         homePage.clearCookiesModal()
                 .verifyHomePageTittle()
-                .userLoginWithInvalidData("<youremail@email.com>","<password>")
+                .userLoginWithInvalidData(email, password)
                 .verifySignInPageTittle()
                 .verifySignInErrorMessage();
     }
 
-    @Test
-    public void LOGIN_userLoginWithUnconfirmedEmails() {
-
+    @Test(dataProvider = "unverifiedUser", dataProviderClass = Data.class)
+    public void LOGIN_userLoginWithUnconfirmedEmails(String email, String password) {
         homePage.clearCookiesModal()
                 .verifyHomePageTittle()
-                .userLoginWithInvalidData("valid@email.com","Valid@123")
+                .userLoginWithInvalidData(email, password)
                 .verifySignInPageTittle()
                 .verifyUnconfirmedEmailMessage();
     }
--- coingecko-web/src/test/java/tests/SearchCoinTest.java
+++ coingecko-web/src/test/java/tests/SearchCoinTest.java
@@ -2,16 +2,19 @@ package tests;
 
 import base.BaseTest;
 import constants.Data;
+import constants.UserRegistry;
+import models.User;
 import org.testng.annotations.Test;
 
 public class SearchCoinTest extends BaseTest {
 
     @Test
     public void SEARCH_trendingCoinDuringSearch() throws Exception {
+        User validUser = new UserRegistry().getUser("valid-user");
 
         homePage.clearCookiesModal()
                 .verifyHomePageTittle()
-                .userLoginWithValidData("confirmemailtester@gmail.com","TestPassword@123")
+                .userLoginWithValidData(validUser.getEmail(), validUser.getPassword())
                 .verifySignedInMessageSuccess()
                 .clickSearchTextBox()
                 .verifyTrendingCoinsExist();
@@ -19,10 +22,11 @@ public class SearchCoinTest extends BaseTest {
 
     @Test(dataProvider = "coins", dataProviderClass = Data.class)
     public void SEARCH_searchCoin(String coins) throws Exception {
+        User validUser = new UserRegistry().getUser("valid-user");
 
         homePage.clearCookiesModal()
                 .verifyHomePageTittle()
-                .userLoginWithValidData("confirmemailtester@gmail.com","TestPassword@123")
+                .userLoginWithValidData(validUser.getEmail(), validUser.getPassword())
                 .verifySignedInMessageSuccess()
                 .clickSearchTextBox()
                 .verifyTrendingCoinsExist()
