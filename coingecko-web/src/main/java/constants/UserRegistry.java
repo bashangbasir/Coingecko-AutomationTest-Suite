9d78cd41c7976e3191877dcd78b3299b4fc57e8d
bashang <bassam.abd.basir@gmail.com>
2023-07-09 22:11:02 +0800

    Create UserRegistry to get user easily

---
+++ coingecko-web/src/main/java/constants/UserRegistry.java
@@ -0,0 +1,28 @@
+package constants;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import models.User;
+
+import java.io.FileReader;
+import java.util.Map;
+
+
+public class UserRegistry {
+    Map<String, User> resultMap;
+
+    public UserRegistry() {
+        Gson gson = new GsonBuilder().create();
+        try{
+            FileReader fileReader = new FileReader("src/main/resources/users.json");
+            this.resultMap = gson.fromJson(fileReader, new TypeToken<Map<String, User>>() {}.getType());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public User getUser(String key){
+        return this.resultMap.get(key);
+    }
+}
---
+++ coingecko-web/src/main/java/models/User.java
@@ -0,0 +1,10 @@
+package models;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String email;
+    private String password;
+}
