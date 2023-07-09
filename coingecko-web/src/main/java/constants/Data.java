de00a39a512c67025c5722ffad7fcc087960d817
bashang <bassam.abd.basir@gmail.com>
2023-07-09 22:09:54 +0800

    Add dataProvider for type of user

--- coingecko-web/src/main/java/constants/Data.java
+++ coingecko-web/src/main/java/constants/Data.java
@@ -8,4 +8,19 @@ public class Data {
     public Object[][] coins(){
         return new Object[][] {{"bitcoin"}, {"xrp"}};
     }
+
+    @DataProvider(name = "invalidUser")
+        public Object[][] invalidUser() {
+        return new Object[][] {{"random.user.email@gmail.com","<password>"}};
+    }
+
+    @DataProvider(name = "validUser")
+    public Object[][] validUser() {
+        return new Object[][] {{"confirmemailtester@gmail.com","TestPassword@123"}};
+    }
+
+    @DataProvider(name = "unverifiedUser")
+    public Object[][] unverifiedUser() {
+        return new Object[][] {{"valid@email.com","Valid@123"}};
+    }
 }
